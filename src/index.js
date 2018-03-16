26e86c959ea395848441d193907d7a72e2c16aab
TorreJohnson <torre.johnson@gmail.com>
2018-03-16 00:21:26 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,96 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
-const Home = () => {
-  return (
+
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+const Navbar = () =>
+  <div>
+    <NavLink
+      to="/"
+      /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+      exact
+      /* add styling to Navlink */
+      style={link}
+      /* add prop for activeStyle */
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Home</NavLink>
+    <NavLink
+      to="/about"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >About</NavLink>
+    <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Login</NavLink>
+  <NavLink
+    to="/signup"
+    exact
+    style={link}
+    activeStyle={{
+      background: 'darkblue'
+    }}
+    >Sign Up</NavLink>
+  <NavLink
+    to="/messages"
+    exact
+    style={link}
+    activeStyle={{
+      background: 'darkblue'
+    }}
+    >Messages</NavLink>
+  </div>;
+
+const Home = () => <h1>Home!</h1>;
+
+const About = () => <h1>This is my about component!</h1>;
+
+const SignUp = () => <h1>Sign Up Page</h1>
+
+const Messages = () => <h1>These are the messages</h1>
+
+const Login = () =>
+  <form>
+    <h1>Login</h1>
     <div>
-      <h1>Home!</h1>
+      <input type="text" name="username" placeholder="Username" />
+      <label htmlFor="username">Username</label>
     </div>
-  );
-};
+    <div>
+      <input type="password" name="password" placeholder="Password" />
+      <label htmlFor="password">Password</label>
+    </div>
+    <input type="submit" value="Login" />
+  </form>;
 
-ReactDOM.render(
-  <Home />,
+ReactDOM.render((
+  <Router>
+    <div>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={SignUp} />
+      <Route exact path="/messages" component={Messages} />
+    </div>
+  </Router>),
   document.getElementById('root')
 );
