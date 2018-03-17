8c4204abe09029184b6d3191b4405f4a154eec5c
Fei <fei.hafferkamp@gmail.com>
2018-03-17 00:34:59 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+import Home from './home';
+import About from './about';
+import Login from './login'
+import Signup from './signup'
 
-const Home = () => {
-  return (
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'lightblue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+const content = {
+  margin: '3rem',
+}
+
+const Navbar = () => (
+  <div>
+    <NavLink to="/" exact style={link} activeStyle={{ background: 'darkblue' }}>Home</NavLink>
+    <NavLink to="/about" exact style={link} activeStyle={{ background: 'darkblue' }}>About</NavLink>
+    <NavLink to="/login" exact style={link} activeStyle={{ background: 'darkblue' }}>Login</NavLink>
+    <NavLink to="/signup" exact style={link} activeStyle={{ background: 'darkblue'}}>Signup</NavLink>
+  </div>
+);
+
+ReactDOM.render((
+  <Router>
     <div>
-      <h1>Home!</h1>
+      <Navbar />
+      <div style={content}>
+        <Route exact path="/" component={ Home } />
+        <Route exact path="/about" component={ About } />
+        <Route exact path="/login" component={ Login } />
+        <Route exact path="/signup" component={ Signup } />
+      </div>
     </div>
-  );
-};
-
-ReactDOM.render(
-  <Home />,
+  </Router>),
   document.getElementById('root')
 );
